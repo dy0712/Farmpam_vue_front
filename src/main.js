4e1fa170f3743c3c9d6015d8a7ad65b1853ce732
kimdayeon <d__y7@naver.com>
2021-09-25 21:46:18 +0900

    commit

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ import axios from "axios"
 import store from "./store"
 
 Vue.config.productionTip = false
-
+ 
 Vue.prototype.$http = axios
 axios.defaults.baseURL = "http://localhost:8888/farmpam"
 new Vue({ store, router, render: (h) => h(App) }).$mount("#app")
